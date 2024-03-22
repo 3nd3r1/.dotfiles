6e8d1b3584b4754f99eac655bf4497cc2439af08
3nd3r1 <ender.spam.guru@gmail.com>
2024-03-22 08:52:46 +0200

    Fix typo in autostart.sh

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -2,7 +2,7 @@
 
 if [ "$(hostname)" == "elx-5cg2183zp2" ];
 then
-    xinpt --set-prop 11 337 -0.8 &
+    xinput --set-prop 11 337 -0.8 &
     feh --bg-fill $HOME/Pictures/Wallpapers/office.png &
     $HOME/.screenlayout/extendOffice.sh &
 else
