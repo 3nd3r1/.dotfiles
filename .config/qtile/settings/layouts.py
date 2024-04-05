a57958b03ddb47c7783581e3a759e248e2112379
3nd3r1 <ender.spam.guru@gmail.com>
2024-04-05 13:48:54 +0300

    qtile: bigger border and margin

--- .config/qtile/settings/layouts.py
+++ .config/qtile/settings/layouts.py
@@ -7,8 +7,8 @@ from .theme import colors
 
 layout_conf = {
     'border_focus': colors['focus'][0],
-    'border_width': 1,
-    'margin': 4
+    'border_width': 2,
+    'margin': 8
 }
 
 layouts = [
