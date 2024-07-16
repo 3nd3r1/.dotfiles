ff860799cca9010baac3e22c107e38854d63290a
3nd3r1 <ender.spam.guru@gmail.com>
2024-07-16 08:41:20 +0300

    zshrc: add fzf and nvim to path

--- .env.zsh
+++ .env.zsh
@@ -4,3 +4,6 @@ alias vi="nvim"
 alias vim="nvim"
 alias oldvim="\vim"
 alias myscrot='scrot ~/Pictures/Screenshots/%b%d::%H%M%S.png'
+
+[ -s "$HOME/.fzf.zsh" ] && source "$HOME/.fzf.zsh"
+export PATH="$PATH:/opt/nvim-linux64/bin"
