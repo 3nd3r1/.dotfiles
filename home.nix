a1a7ec580199c9624a228b02604ea6d2eb939a0b
3nd3r1 <ender.spam.guru@gmail.com>
2025-09-02 10:44:28 +0300

    nix: update home

--- home.nix
+++ home.nix
@@ -15,27 +15,6 @@
   # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
@@ -72,9 +51,17 @@
     # EDITOR = "emacs";
   };
 
-	services.ssh-agent = {
-	  enable = true;
-	};
+    services.ssh-agent = {
+      enable = true;
+    };
+
+  home.packages = with pkgs; [
+	ripgrep
+	python3
+	gcc
+    nodejs_24
+	yaml-language-server
+  ];
 
   home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink
     "${config.home.homeDirectory}/dotfiles/nvim";
