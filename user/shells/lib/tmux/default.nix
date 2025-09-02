71852d5524782aa6400e80c799c2fbf46b44e7dc
3nd3r1 <ender.spam.guru@gmail.com>
2025-09-02 17:35:16 +0300

    zsh: Add tmux

---
+++ user/shells/lib/tmux/default.nix
@@ -0,0 +1,91 @@
+{ pkgs, settings, ... }: {
+  programs.tmux = {
+    enable = true;
+    prefix = "C-a";
+    terminal = "tmux-256color";
+    escapeTime = 0;
+    mouse = true;
+    historyLimit = 10000;
+    plugins = with pkgs.tmuxPlugins; [
+      vim-tmux-navigator
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-save 'C-F1'
+          set -g @resurrect-restore 'C-F2'
+        '';
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+        '';
+      }
+      sensible
+      prefix-highlight
+      yank
+      tmux-fzf
+      sysstat
+      #tmux-window-name
+      #kube-tmux
+    ];
+    extraConfig = ''
+      # Tmux nesting
+      if-shell 'test -n "$SSH_CLIENT"' 'unbind F12'
+      bind -T root F12  \
+          set prefix None \;\
+          set key-table off \;\
+          set status off \;\
+          if -F '#{pane_in_mode}' 'send-keys -X cancel' \;\
+          refresh-client -S \;\
+
+      bind -T off F12 \
+        set -u prefix\;\
+        set -u status\;\
+        set -u key-table \;\
+        refresh-client -S
+
+      ####################
+      #     Shortcuts    #
+      ####################
+
+      # new window and retain cwd
+      bind c new-window -c "#{pane_current_path}"
+
+      # Rename session and window
+      bind r command-prompt -I "#{window_name}" "rename-window '%%'"
+      bind R command-prompt -I "#{session_name}" "rename-session '%%'"
+
+      # Split panes
+      bind | split-window -h -c "#{pane_current_path}"
+      bind _ split-window -v -c "#{pane_current_path}"
+
+      # Select pane and windows
+      bind -r k select-pane -U
+      bind -r j select-pane -D
+      bind -r h select-pane -L
+      bind -r l select-pane -R
+      bind -r C-o swap-pane -D
+
+      # Copy mode
+      bind q copy-mode
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+
+      # Zoom pane
+      bind + resize-pane -Z
+
+      # Kill pane/window/session shortcuts
+      bind x kill-pane
+      bind X kill-window
+
+      # Detach from session
+      bind d detach
+
+      # Cht
+      bind-key -r i run-shell "tmux neww cht.sh --shell"
+
+      # Session switcher
+      bind-key -r f run-shell -b "$HOME/.config/tmux/plugins/tmux-fzf/scripts/session.sh switch"
+    '';
+  };
+}
--- user/shells/zsh.nix
+++ user/shells/zsh.nix
@@ -1,7 +1,7 @@
 { settings, config, pkgs, lib, ... }:
 
 {
-
+  imports = [ ./lib/tmux ];
   # Move the custom directory
   home.file.".config/oh-my-zsh".source = config.lib.file.mkOutOfStoreSymlink
     "${config.home.homeDirectory}/dotfiles/oh-my-zsh";
@@ -35,6 +35,30 @@
         "rust"
         "deno"
       ];
+      extraConfig = ''
+        zstyle ':completion:*' menu select
+        zmodload zsh/complist
+        bindkey -M menuselect 'h' vi-backward-char
+        bindkey -M menuselect 'k' vi-up-line-or-history
+        bindkey -M menuselect 'l' vi-forward-char
+        bindkey -M menuselect 'j' vi-down-line-or-history
+
+        ZSH_TMUX_AUTOSTART=true
+        ZSH_TMUX_AUTOSTART_ONCE=false
+        ZSH_TMUX_AUTOQUIT=true
+        ZSH_TMUX_CONFIG="$XDG_CONFIG_HOME/tmux/tmux.conf"
+        ZSH_TMUX_DEFAULT_SESSION_NAME="terminal"
+
+        # Fzf settings
+        DISABLE_FZF_AUTO_COMPLETION="true"
+        DISABLE_FZF_KEY_BINDINGS="false"
+        FZF_CTRL_T_COMMAND='find -L . -mindepth 1'
+        FZF_ALT_C_COMMAND='find -L . -type d -mindepth 1'
+
+        fpath=($ZSH_CUSTOM/completions $fpath)
+
+        bindkey '^ ' autosuggest-accept
+      '';
     };
   };
 }
