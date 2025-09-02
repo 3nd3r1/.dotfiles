6a93a8a7d3ceaf418e205965c2a3710b4df3dbdf
3nd3r1 <ender.spam.guru@gmail.com>
2025-09-02 09:31:45 +0300

    chore: Creating a minimal flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "My minimal NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.enderlaptop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          # Replace 'yourusername' with your actual username
+          home-manager.users.ender = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
