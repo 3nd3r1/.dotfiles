3db453b24750917f0324a30536bb667f1ddd5d1e
3nd3r1 <ender.spam.guru@gmail.com>
2025-09-03 18:04:53 +0300

    hyprland: fix config

--- user/wm/hyprland.nix
+++ user/wm/hyprland.nix
@@ -11,7 +11,6 @@
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
     portalPackage =
       inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
-    settings = {
-    };
+    extraConfig = (builtins.readFile ./hyprland.conf);
   };
 }
