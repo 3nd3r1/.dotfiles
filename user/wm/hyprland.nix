1030f16052b4553bd58fa9a03afc34be55ee6def
3nd3r1 <ender.spam.guru@gmail.com>
2025-09-03 19:18:03 +0300

    hyprland: remove old config

--- user/wm/hyprland.nix
+++ user/wm/hyprland.nix
@@ -1,8 +1,6 @@
 { inputs, config, lib, settings, pkgs, ... }:
 
 {
-  imports = [ ./hyprland/binds.nix ];
-
   home.packages = with pkgs; [ hyprcursor rose-pine-hyprcursor ];
 
   wayland.windowManager.hyprland = {
