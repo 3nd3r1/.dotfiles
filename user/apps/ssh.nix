e9eb586b860e6e4cfa92a85b7311fb281d75085d
3nd3r1 <ender.spam.guru@gmail.com>
2025-09-05 09:45:44 +0300

    ssh: add work ssh config

--- user/apps/ssh.nix
+++ user/apps/ssh.nix
@@ -1,9 +1,29 @@
-{ pkgs, ... }: {
+{ pkgs, settings, ... }:
+let
+  profileBlocks = {
+    laptop = { };
+    work = {
+      "devenv" = {
+        hostname = "node-100-77-161-117.rnd.gic.ericsson.se";
+        user = "eavinrl";
+        identityFile = "~/.ssh/id_eavinrl";
+      };
+      "devenvold" = {
+        hostname = "node-100-77-161-117.rnd.gic.ericsson.se";
+        user = "eavinrl";
+        identityFile = "~/.ssh/id_eavinrl";
+      };
+    };
+  };
+  blocks = {
+    "*" = { identityFile = "~/.ssh/id_3nd3r1"; };
+  } // (profileBlocks.${settings.profile} or { });
+in {
   services.ssh-agent.enable = true;
 
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks."*" = { identityFile = "~/.ssh/id_3nd3r1"; };
+    matchBlocks = blocks;
   };
 }
