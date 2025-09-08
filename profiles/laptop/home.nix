dbb7b60bacb3af686bd8bdde53a191d252d230ed
3nd3r1 <ender.spam.guru@gmail.com>
2025-09-08 15:27:34 +0300

    latop: remove netrc

--- profiles/laptop/home.nix
+++ profiles/laptop/home.nix
@@ -16,7 +16,7 @@ in {
     homeDirectory = "/home/${settings.username}";
   };
 
-  home.packages = with pkgs; [ neofetch docker netrc ];
+  home.packages = with pkgs; [ neofetch docker ];
 
   home.sessionVariables = {
     EDITOR = settings.preferredEditor;
