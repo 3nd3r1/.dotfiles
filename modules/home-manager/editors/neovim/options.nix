3d18697307275b173b6ea351308af3c60c9f071d
3nd3r1 <ender.spam.guru@gmail.com>
2025-09-16 09:08:23 +0300

    neovim: fix the clipboard

--- modules/home-manager/editors/neovim/options.nix
+++ modules/home-manager/editors/neovim/options.nix
@@ -1,11 +1,15 @@
 {
-  programs.nixvim.opts = {
-    number = true;
-    relativenumber = true;
-    shiftwidth = 4;
-    tabstop = 4;
-    expandtab = true;
-    undofile = true;
-    signcolumn = "yes";
+  programs.nixvim = {
+    clipboard.register = "unnamedplus";
+
+    opts = {
+      number = true;
+      relativenumber = true;
+      shiftwidth = 4;
+      tabstop = 4;
+      expandtab = true;
+      undofile = true;
+      signcolumn = "yes";
+    };
   };
 }
