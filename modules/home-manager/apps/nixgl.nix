97a51d10a3e1603fc817dd7a11b216b4a82daa6a
3nd3r1 <ender.spam.guru@gmail.com>
2025-09-19 13:59:40 +0300

    nixgl: add nonixgl script

--- modules/home-manager/apps/nixgl.nix
+++ modules/home-manager/apps/nixgl.nix
@@ -1,8 +1,22 @@
-{ nixGL, ... }:
-
+{ nixGL, pkgs, ... }:
+let
+  nonixgl = pkgs.writeShellScriptBin "nonixgl" ''
+    exec env \
+      -u LD_LIBRARY_PATH \
+      -u LIBGL_DRIVERS_PATH \
+      -u LIBVA_DRIVERS_PATH \
+      -u __EGL_VENDOR_LIBRARY_FILENAMES \
+      -u GBM_BACKENDS_PATH \
+      "$@"
+  '';
+in
 {
   nixGL = {
     packages = nixGL.packages;
     defaultWrapper = "mesa";
   };
+
+  home.packages = [
+    nonixgl
+  ];
 }
