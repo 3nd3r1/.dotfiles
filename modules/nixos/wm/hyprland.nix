a6d3c672f5f1acceb46065f329a2e8fed231bb4b
3nd3r1 <ender.spam.guru@gmail.com>
2025-09-22 13:52:23 +0300

    hyprland: make sddm display on external

--- modules/nixos/wm/hyprland.nix
+++ modules/nixos/wm/hyprland.nix
@@ -23,6 +23,19 @@
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    setupScript = ''
+      # Get connected displays
+      EXTERNAL=$(${pkgs.xorg.xrandr}/bin/xrandr | grep " connected" | grep -v "eDP" | head -n1 | awk '{print $1}')
+      INTERNAL=$(${pkgs.xorg.xrandr}/bin/xrandr | grep "eDP" | grep " connected" | awk '{print $1}')
+
+      # If external display is connected, use it as primary and turn off internal
+      if [ -n "$EXTERNAL" ]; then
+        ${pkgs.xorg.xrandr}/bin/xrandr --output "$EXTERNAL" --primary --auto
+        if [ -n "$INTERNAL" ]; then
+          ${pkgs.xorg.xrandr}/bin/xrandr --output "$INTERNAL" --off
+        fi
+      fi
+    '';
   };
 
   programs = {
