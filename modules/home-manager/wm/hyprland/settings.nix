6171054b0505d327c8241908571860ceb91bcd94
3nd3r1 <ender.spam.guru@gmail.com>
2025-09-29 12:04:09 +0300

    hyprland: fix monitor setup

--- modules/home-manager/wm/hyprland/settings.nix
+++ modules/home-manager/wm/hyprland/settings.nix
@@ -9,9 +9,9 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "DP-1,preferred,0x0,1"
-      "HDMI-A-1,preferred,0x0,1"
-      "eDP-1,preferred,auto,1"
+      "eDP-1,preferred,0x0,1"
+      "DP-1,preferred,1920x0,1"
+      "HDMI-A-1,preferred,1920x0,1"
       ",preferred,auto,1"
     ];
 
