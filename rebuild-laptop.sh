e24323f9bd215ab5f7af1cc46b1511b11121fdf1
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-06 11:40:45 +0300

    laptop: fix laptop rebuild command

--- rebuild-laptop.sh
+++ rebuild-laptop.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 sudo nixos-rebuild switch --flake ~/dotfiles#laptop
-home-manager switch --flake .#laptop -b backup
 
+home-manager switch \
+    --flake .#laptop \
+    --extra-experimental-features nix-command \
+    --extra-experimental-features flakes \
+    --extra-experimental-features pipe-operators \
+    -b backup
