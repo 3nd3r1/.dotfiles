d15bb434ecac0677f9584fba67e34738cf380ec8
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-15 09:12:49 +0300

    kitty: make ctrl+l work

--- modules/home-manager/terminals/kitty.nix
+++ modules/home-manager/terminals/kitty.nix
@@ -48,6 +48,8 @@ in
       "ctrl+shift+minus" = "decrease_font_size";
       "ctrl+shift+backspace" = "restore_font_size";
       "ctrl+shift+f6" = "set_font_size 16.0";
+
+      "ctrl+l" = "no_op";
     };
   };
 }
