e2f2df03d236a3623cfc7ddd40f01e1a55339d46
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-23 13:36:56 +0300

    teams: add some teams configs

--- modules/home-manager/apps/teams.nix
+++ modules/home-manager/apps/teams.nix
@@ -4,4 +4,12 @@
 }:
 {
   home.packages = with pkgs; [ teams-for-linux ];
+
+  xdg.configFile."teams-for-linux/config.json".text = ''
+    {
+      "ssoInTuneEnabled": true,
+      "ssoInTuneAuthUser": "viljami.ranta@ericsson.com",
+      "notificationMethod": "electron"
+    }
+  '';
 }
