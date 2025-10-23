7cbf24232b25db91fdb7575b44c0313965651746
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-23 09:15:43 +0300

    brave: fix brave

--- modules/home-manager/browsers/brave.nix
+++ modules/home-manager/browsers/brave.nix
@@ -7,10 +7,7 @@
     enable = true;
     package = pkgs.brave;
     commandLineArgs = [
-      "--enable-features=UseOzonePlatform,WaylandWindowDecorations,WaylandTextInputV3"
-      "--wayland-text-input-version=3"
-      "--ozone-platform=wayland"
-      "--enable-wayland-ime=true"
+      "--disable-features=WaylandAsyncClipboard"
     ];
   };
 }
