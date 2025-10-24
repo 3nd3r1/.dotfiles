1d88cd5c067045f1efaadcbe1581c014149af9d3
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-24 08:08:59 +0000

    neovim: add yamllint config

--- modules/home-manager/editors/neovim/plugins/lint.nix
+++ modules/home-manager/editors/neovim/plugins/lint.nix
@@ -4,6 +4,20 @@
     yamllint
   ];
 
+  xdg.configFile."yamllint/config".text = ''
+    extends: default
+
+    rules:
+      line-length:
+        max: 120
+        level: warning
+      indentation:
+        spaces: 2
+      document-start: disable
+      truthy:
+        allowed-values: ['true', 'false', 'yes', 'no']
+  '';
+
   programs.nixvim.plugins = {
     lint = {
       enable = true;
