1d17db152e49a4579aacae07d2cf08b37115d6de
3nd3r1 <ender.spam.guru@gmail.com>
2025-10-24 10:04:44 +0300

    neovim: add bg to notify

--- modules/home-manager/editors/neovim/plugins/ui/default.nix
+++ modules/home-manager/editors/neovim/plugins/ui/default.nix
@@ -3,7 +3,12 @@
 
   programs.nixvim.plugins = {
     web-devicons.enable = true;
-    notify.enable = true;
+    notify = {
+      enable = true;
+      settings = {
+        background_colour = "#000000";
+      };
+    };
     which-key.enable = true;
     alpha = {
       enable = true;
