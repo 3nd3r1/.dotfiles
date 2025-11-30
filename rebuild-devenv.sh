bb94ddadddf2122d4bc26ea5d0db3177dced0d9e
3nd3r1 <ender.spam.guru@gmail.com>
2025-11-30 10:24:30 +0200

    chore: remove old shell scripts

--- rebuild-devenv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-home-manager switch \
-    --flake .#devenv --extra-experimental-features nix-command \
-    --extra-experimental-features flakes \
-    --extra-experimental-features pipe-operators \
-    -b backup
--- rebuild-work.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-home-manager switch \
-    --flake .#work --extra-experimental-features nix-command \
-    --extra-experimental-features flakes \
-    --extra-experimental-features pipe-operators \
-    -b backup
