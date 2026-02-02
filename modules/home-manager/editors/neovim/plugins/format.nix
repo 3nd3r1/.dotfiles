9c8b1e76d6471b2ec8bd5f8015c8e0710722129b
3nd3r1 <ender.spam.guru@gmail.com>
2026-02-02 16:23:23 +0200

    neovim: disable robotidy for now

--- modules/home-manager/editors/neovim/plugins/format.nix
+++ modules/home-manager/editors/neovim/plugins/format.nix
@@ -10,7 +10,7 @@ in
   home.packages = with pkgs; [
     stylua
     prettier
-    robotframework-tidy
+    # robotframework-tidy  # Commented out due to test failures
     go
     beautysh
     nixfmt
