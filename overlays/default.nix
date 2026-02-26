d0e9f55fc50045208595d3faa1e3ea579a2de179
3nd3r1 <ender.spam.guru@gmail.com>
2026-02-26 15:45:19 +0200

    kiro: fix the hash

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,4 +1,9 @@
 _: final: prev: {
+  kiro-cli = prev.kiro-cli.overrideAttrs (oldAttrs: {
+    src = oldAttrs.src.overrideAttrs {
+      outputHash = "sha256-q5ixEPVXL9VHtCKCpCUIAd426k7HGewpuXDWfB0q06c=";
+    };
+  });
   material-black-plum-suru-icons = final.callPackage ../pkgs/material-black-plum-suru-icons { };
   material-black-plum-be-colors = final.callPackage ../pkgs/material-black-plum-be-colors { };
   steelfish-outline-regular-font = final.callPackage ../pkgs/steelfish-outline-regular-font { };
