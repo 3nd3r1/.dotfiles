24f55304c7b63a49bdb9dfc215a4f4a8b1f45c5d
3nd3r1 <ender.spam.guru@gmail.com>
2026-04-23 12:14:27 +0300

    neovim: update deprecated treesitter settings

--- modules/home-manager/editors/neovim/plugins/treesitter.nix
+++ modules/home-manager/editors/neovim/plugins/treesitter.nix
@@ -1,35 +1,10 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  home.packages = with pkgs; [
-    gcc
-  ];
   programs.nixvim.plugins.treesitter = {
     enable = true;
-    settings = {
-      # A list of parser names, or "all"
-      ensure_installed = [
-        "typescript"
-        "javascript"
-        "python"
-        "c"
-        "lua"
-        "vim"
-        "vimdoc"
-        "query"
-        "bash"
-        "yaml"
-        "go"
-        "nix"
-        "json"
-        "markdown"
-      ];
-
-      auto_install = true;
-
-      highlight = {
-        enable = true;
-        additional_vim_regex_highlighting = false;
-      };
+    highlight = {
+      enable = true;
+      additional_vim_regex_highlighting = false;
     };
   };
 }
