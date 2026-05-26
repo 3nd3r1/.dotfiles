247735eda2715fbb76686e96514850361460087b
3nd3r1 <ender.spam.guru@gmail.com>
2026-05-26 11:30:18 +0100

    aerospace: make aero config more hyprland like

--- modules/home-manager/wm/aerospace/default.nix
+++ modules/home-manager/wm/aerospace/default.nix
@@ -1,4 +1,14 @@
-_: {
+{ pkgs, ... }:
+let
+  workspaceHere = pkgs.writeShellScript "aerospace-workspace-here" ''
+    export PATH=/opt/homebrew/bin:/usr/local/bin:$PATH
+    target="$1"
+    mon="$(aerospace list-monitors --focused --format '%{monitor-id}')"
+    aerospace workspace "$target"
+    aerospace move-workspace-to-monitor "$mon"
+  '';
+in
+{
   xdg.configFile."aerospace/aerospace.toml".text = ''
     start-at-login = true
     after-login-command = []
@@ -16,17 +26,16 @@ _: {
     outer.right = 8
 
     [mode.main.binding]
-    ctrl-alt-shift-cmd-slash = 'layout tiles horizontal vertical'
-    ctrl-alt-shift-cmd-comma = 'layout accordion horizontal vertical'
-    ctrl-alt-shift-cmd-f = 'fullscreen'
+    alt-slash = 'layout tiles horizontal vertical'
+    alt-f = 'fullscreen'
 
-    ctrl-alt-shift-cmd-q = 'close'
-    ctrl-alt-shift-cmd-enter = 'exec-and-forget open -n /Applications/kitty.app'
+    alt-q = 'close'
+    alt-enter = 'exec-and-forget open -n /Applications/kitty.app'
 
-    ctrl-alt-shift-cmd-h = 'focus left'
-    ctrl-alt-shift-cmd-j = 'focus down'
-    ctrl-alt-shift-cmd-k = 'focus up'
-    ctrl-alt-shift-cmd-l = 'focus right'
+    alt-h = 'focus left'
+    alt-j = 'focus down'
+    alt-k = 'focus up'
+    alt-l = 'focus right'
 
     alt-shift-h = 'move left'
     alt-shift-j = 'move down'
@@ -36,17 +45,30 @@ _: {
     alt-shift-minus = 'resize smart -50'
     alt-shift-equal = 'resize smart +50'
 
-    ctrl-alt-shift-cmd-1 = 'workspace 1'
-    ctrl-alt-shift-cmd-2 = 'workspace 2'
-    ctrl-alt-shift-cmd-3 = 'workspace 3'
-    ctrl-alt-shift-cmd-4 = 'workspace 4'
-    ctrl-alt-shift-cmd-5 = 'workspace 5'
+    alt-comma = 'focus-monitor --wrap-around prev'
+    alt-period = 'focus-monitor --wrap-around next'
+    alt-shift-comma = 'move-node-to-monitor --wrap-around prev'
+    alt-shift-period = 'move-node-to-monitor --wrap-around next'
+
+    alt-1 = 'exec-and-forget ${workspaceHere} 1'
+    alt-2 = 'exec-and-forget ${workspaceHere} 2'
+    alt-3 = 'exec-and-forget ${workspaceHere} 3'
+    alt-4 = 'exec-and-forget ${workspaceHere} 4'
+    alt-5 = 'exec-and-forget ${workspaceHere} 5'
+    alt-6 = 'exec-and-forget ${workspaceHere} 6'
+    alt-7 = 'exec-and-forget ${workspaceHere} 7'
+    alt-8 = 'exec-and-forget ${workspaceHere} 8'
+    alt-9 = 'exec-and-forget ${workspaceHere} 9'
 
     alt-shift-1 = 'move-node-to-workspace 1'
     alt-shift-2 = 'move-node-to-workspace 2'
     alt-shift-3 = 'move-node-to-workspace 3'
     alt-shift-4 = 'move-node-to-workspace 4'
     alt-shift-5 = 'move-node-to-workspace 5'
+    alt-shift-6 = 'move-node-to-workspace 6'
+    alt-shift-7 = 'move-node-to-workspace 7'
+    alt-shift-8 = 'move-node-to-workspace 8'
+    alt-shift-9 = 'move-node-to-workspace 9'
 
     alt-shift-semicolon = 'mode service'
 
