94b3f529404432f80e97fb33c849ba6db882f032
3nd3r1 <ender.spam.guru@gmail.com>
2026-05-26 09:40:07 +0100

    git: enable lfs and gpg signing on work

--- modules/home-manager/apps/git.nix
+++ modules/home-manager/apps/git.nix
@@ -1,9 +1,16 @@
-{ settings, ... }:
+{ settings, lib, ... }:
 
+let
+  isWork = settings.profile == "work";
+in
 {
   programs.git = {
     enable = true;
-    lfs.enable = true;
+    lfs.enable = isWork;
+    signing = lib.mkIf isWork {
+      key = "F964ED111357DE83";
+      signByDefault = true;
+    };
     settings = {
       user.name = settings.name;
       user.email = settings.email;
