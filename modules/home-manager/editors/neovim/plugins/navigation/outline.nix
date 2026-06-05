45f37f787c4dc96ee28f44bfd41ee8501e6924f7
3nd3r1 <ender.spam.guru@gmail.com>
2026-06-05 10:05:17 +0100

    nvim: use aerial for outline

--- modules/home-manager/editors/neovim/plugins/navigation/outline.nix
+++ modules/home-manager/editors/neovim/plugins/navigation/outline.nix
@@ -1,13 +1,31 @@
 {
-  programs.nixvim.plugins.outline = {
+  programs.nixvim.plugins.aerial = {
     enable = true;
+    settings = {
+      backends = [
+        "treesitter"
+        "lsp"
+        "markdown"
+        "man"
+      ];
+      layout = {
+        default_direction = "right";
+        min_width = 30;
+        max_width = [
+          0.25
+          50
+        ];
+      };
+      filter_kind = false;
+      show_guides = true;
+    };
   };
 
   programs.nixvim.keymaps = [
     {
       mode = "n";
-      key = "<leader>o";
-      action = "<cmd>Outline<CR>";
+      key = "<leader>mo";
+      action = "<cmd>AerialToggle!<CR>";
       options = {
         desc = "Toggle outline sidebar";
         silent = true;
