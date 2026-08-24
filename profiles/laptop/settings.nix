b68d59f6a2c0bdda13801258d434e32062a1ca13
3nd3r1 <ender.spam.guru@gmail.com>
2026-08-24 14:34:15 +0100

    laptop: Update timezone to work

--- profiles/laptop/settings.nix
+++ profiles/laptop/settings.nix
@@ -4,7 +4,7 @@ rec {
 
   hostname = "enderlaptop"; # Hostname
   username = "ender"; # Username
-  timezone = "Europe/London"; # Select timezone
+  timezone = "Europe/Helsinki"; # Select timezone
   locale = "en_US.UTF-8"; # Select locale
   name = "3nd3r1"; # Name (git config)
   email = "ender.spam.guru@gmail.com"; # Email (git config)
