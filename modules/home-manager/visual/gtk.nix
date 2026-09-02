44f5c23f1668df74e846eaa31cc0aa70a9b80c42
3nd3r1 <ender.spam.guru@gmail.com>
2026-09-02 10:07:59 +0300

    visual: fix gtk

--- modules/home-manager/visual/gtk.nix
+++ modules/home-manager/visual/gtk.nix
@@ -9,6 +9,11 @@ lib.mkIf pkgs.stdenv.hostPlatform.isLinux {
     enable = true;
     iconTheme = lib.mkForce themeDetails.icons;
     theme = lib.mkForce themeDetails.theme;
-    gtk4.theme = lib.mkForce themeDetails.theme;
+    # Do NOT import the GTK3 theme's CSS into GTK4: Material-Black-Plum has no
+    # libadwaita support and wrecks GNOME 50 apps (nautilus, papers, ...).
+    # GTK3 apps (thunar) stay themed; libadwaita apps use Adwaita, forced dark.
+    gtk4.theme = lib.mkForce null;
   };
+
+  dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
 }
