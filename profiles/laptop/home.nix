a8d7d8baff63b1bc27de15a845edb066f933f2c9
3nd3r1 <ender.spam.guru@gmail.com>
2026-09-21 14:15:15 +0300

    laptop: use loupe for default image

--- profiles/laptop/home.nix
+++ profiles/laptop/home.nix
@@ -65,6 +65,17 @@ in
     defaultApplications = {
       "inode/directory" = "thunar.desktop";
       "application/x-directory" = "thunar.desktop";
+
+      "image/png" = "org.gnome.Loupe.desktop";
+      "image/jpeg" = "org.gnome.Loupe.desktop";
+      "image/gif" = "org.gnome.Loupe.desktop";
+      "image/webp" = "org.gnome.Loupe.desktop";
+      "image/bmp" = "org.gnome.Loupe.desktop";
+      "image/tiff" = "org.gnome.Loupe.desktop";
+      "image/svg+xml" = "org.gnome.Loupe.desktop";
+      "image/avif" = "org.gnome.Loupe.desktop";
+      "image/heic" = "org.gnome.Loupe.desktop";
+      "image/x-icon" = "org.gnome.Loupe.desktop";
     };
   };
 
